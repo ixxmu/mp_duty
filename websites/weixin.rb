b37269fbd942f781ba8c378c9887979942b28a30
ixxmu <ixxmu@users.noreply.github.com>
2024-05-08 11:17:21 +0800

    Update weixin.rb

--- websites/weixin.rb
+++ websites/weixin.rb
@@ -5,14 +5,11 @@ register_website(
     uri.hostname == 'mp.weixin.qq.com'
   },
   request: ->(uri) {
-    uri = if uri.scheme == 'https'
-            uri
-          else
-            uri.scheme = 'https'
-            URI(uri.to_s)
-          end
+    headers = {
+      'User-Agent' => 'Mozilla/5.0 (Linux; Android 11; KB2000 Build/RP1A.201005.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160117 MMWEBSDK/20240404 MMWEBID/2484 MicroMessenger/8.0.49.2600(0x28003137) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64'
+    }
 
-    get_with_headers(uri, {})
+    get_with_headers(uri, headers)
   },
   process: -> (html) {
     document = Nokogiri::HTML(html)
